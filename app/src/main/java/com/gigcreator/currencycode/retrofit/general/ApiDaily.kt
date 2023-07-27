13adc66b68ded7b01000b9816a2b7026a39423ed
Vladimir <117392112+Vladimir-Olegovych@users.noreply.github.com>
2023-07-27 10:57:51 +0300

    Delete app/src directory

--- app/src/main/java/com/gigcreator/currencycode/retrofit/general/ApiDaily.kt
+++
@@ -1,9 +0,0 @@
-package com.gigcreator.currencycode.retrofit.general
-
-import com.gigcreator.currencycode.retrofit.api.Daily
-import retrofit2.http.GET
-
-interface ApiDaily {
-    @GET("daily_json.js")
-    suspend fun getDaily(): Daily
-}
